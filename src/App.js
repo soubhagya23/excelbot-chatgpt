7c95ed39ddbebe6923b1cd27a4e77c9353d603ce
soubhagya23 <ranjansoubhagya.master@gmail.com>
2023-01-18 20:00:37 +0530

    Homepage Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import ChatGPT from './Components/apidemo';
+import Homepage from './Components/Homepage/main';
 
 function App() {
   return (
    <ChatGPT />
+  /* <Homepage /> */
   );
 }
 
 export default App;
+
