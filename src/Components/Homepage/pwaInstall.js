843f448483ce20774535b6aa6ff79ac8243ffa1d
soubhagya23 <ranjansoubhagya.master@gmail.com>
2023-01-30 19:26:31 +0530

    Done Fixing Service Woker

--- src/Components/Homepage/pwaInstall.js
+++ src/Components/Homepage/pwaInstall.js
@@ -1,15 +1,33 @@
-import React from 'react'
-import { usePWAInstall } from 'react-use-pwa-install'
+import React from "react";
+import { usePWAInstall } from "react-use-pwa-install";
+import Button from "@mui/material/Button";
+import { FcApproval } from "react-icons/fc";
 
 export const PwaIns = () => {
-	const install = usePWAInstall()
+  const install = usePWAInstall();
 
-	return (
-		<header>
-			<h1>My app</h1>
-			{install && <button onClick={install}>Install</button>}
-		</header>
-	)
-}
+  return (
+    <header>
+      
+			{install && <Button
+        variant="outlined"
+        onClick={install}
+        style={{
+          marginBottom: "20px",
+          backgroundColor: "#05182b",
+          color: "white",
+          textTransform: "none",
+          fontFamily: "Inter",
+          padding: "12px",
+          borderRadius:'32px'
+        }}
+      >
+        <FcApproval style={{ marginRight: "10px" }} size={23} />
+        Install App
+      </Button>} 
+      
+    </header>
+  );
+};
 
 export default PwaIns;
