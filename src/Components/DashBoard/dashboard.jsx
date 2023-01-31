b7ab3b657465aaa68abcd86a4c266173cdcf8360
aditya-majhi <majhi2723@gmail.com>
2023-01-31 19:57:13 +0530

    Chat UI fixed

--- src/Components/DashBoard/dashboard.jsx
+++ src/Components/DashBoard/dashboard.jsx
@@ -72,10 +72,10 @@ const DashBorad = () => {
 
   return (
     <>
-      <div className="flex-1 sm:p-6 justify-between flex flex-col h-screen w-4/5 mx-auto">
-        <div id="messages" className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch mt-2 pt-3 h-3/4">
+      <div className="flex-1 sm:p- justify-between flex flex-col h-screen md:w-4/5 mx-auto">
+        <div id="messages" className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch mt-4 md:mt-6 h-3/4">
           <div className="chat-message pt-5">
-            <div className="flex items-end">
+            <div className="flex items-end pt-2">
               <div className="flex flex-col space-y-2 text-sm max-w-xs mx-2 order-2 items-start">
                 <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">Type Anything..</span></div>
               </div>
@@ -91,13 +91,13 @@ const DashBorad = () => {
           }
         </div>
 
-        <div className="px-4 pt-4 mb-4 sm:mb-0">
-          <div className="flex flex-col md:flex-row space-x-2">
-            <input type="text" placeholder="Write your message!" className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3 border" onChange={handleQuestionChange} />
+        <div className="px-4 pt-4 mb-4">
+          <div className="flex flex-col md:flex-row space-x-2 mb-4">
+            <input type="text" placeholder="Write your message!" className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-8 bg-gray-200 rounded-md py-2 border" onChange={handleQuestionChange} />
             <div className="items-center">
-              <button type="button" className="inline-flex items-center justify-center rounded-lg px-4 sm:py-1.5 md:py-[15px] transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none w-full space-x-1" onClick={handleClick}>
+              <button type="button" className="inline-flex items-center justify-center rounded-lg px-4 py-[7px] transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none w-full space-x-1" onClick={handleClick}>
                 <span className="font-bold md:text-lg">{loading ? "Generating.." : "Generate"}</span>
-                {loading ? null : <RiSendPlane2Fill size='25px' />}
+                {loading ? null : <RiSendPlane2Fill size='20px' />}
               </button>
             </div>
           </div>
