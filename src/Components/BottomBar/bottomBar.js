ec9d3e751d57577c6f8228e3f7b04c4ec55686a0
aditya-majhi <majhi2723@gmail.com>
2023-02-01 16:03:05 +0530

    navbar updated

--- src/Components/BottomBar/bottomBar.js
+++ src/Components/BottomBar/bottomBar.js
@@ -3,6 +3,7 @@ import { MdMessage } from 'react-icons/md'
 import { BsPlusCircleFill } from 'react-icons/bs'
 import { CgProfile } from 'react-icons/cg'
 import { Link } from "react-router-dom";
+import { Popover, PopoverHandler, PopoverContent, Button } from "@material-tailwind/react";
 
 const FixedBottomNavigation = () => {
   return (
@@ -10,15 +11,22 @@ const FixedBottomNavigation = () => {
       <section id="bottom-navigation" className="block fixed inset-x-0 mt-3 z-10 bottom-0 bg-gray-200 shadow-lg w-full mx-auto">
         <div id="tabs" className="flex justify-evenly">
           <div className="w-full focus:text-blue-400 justify-center inline-block text-center pt-2 pb-1">
-            <Link to='/dashboard/chats'><span className="tab tab-home text-xs"><MdMessage size='25px' className="mx-auto hover:text-blue-400"/></span></Link>
+            <Link to='/dashboard/chats'><span className="text-xs"><MdMessage size='25px' className="mx-auto hover:text-blue-400" /></span></Link>
           </div>
 
           <div className="w-full focus:text-blue-400 justify-center inline-block text-center pt-2 pb-1">
-            <span className="tab tab-kategori block text-xs"><BsPlusCircleFill size='25px' className="mx-auto hover:text-blue-400"/></span>
+            <span className="block text-xs"><BsPlusCircleFill size='25px' className="mx-auto hover:text-blue-400" /></span>
           </div>
 
           <div className="w-full focus:text-blue-400 justify-center inline-block text-center pt-2 pb-1">
-            <span className="tab tab-explore block text-xs"><CgProfile size='25px' className="mx-auto  hover:text-blue-400"/></span>
+            <Popover>
+              <PopoverHandler>
+                <span className="block text-xs"><CgProfile size='25px' className="mx-auto  hover:text-blue-400" /></span>
+              </PopoverHandler>
+              <PopoverContent className="">
+                profile
+              </PopoverContent>
+            </Popover>
           </div>
         </div>
       </section>
