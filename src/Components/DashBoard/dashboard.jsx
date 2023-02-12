e05e300cf3a1e8834307fe203c8d306d16212955
aditya-majhi <majhi2723@gmail.com>
2023-02-12 11:38:39 +0530

    Added functionalities in Dashboard

--- src/Components/DashBoard/dashboard.jsx
+++ src/Components/DashBoard/dashboard.jsx
@@ -16,10 +16,14 @@ const DashBorad = () => {
   const openai = new OpenAIApi(configuration);
 
   const [quesAns, setQuesAns] = useState(localStorage.response ? JSON.parse(localStorage.getItem('response')) : []);
-  const [currQues, setCurrQues] = useState('Write an Excel formula');
   const [loading, setLoading] = useState(false);
   const [python, setPython] = useState(false);
-  const [excel, setExcel] = useState(true) ;
+  const [excel, setExcel] = useState(true);
+  const [currQues, setCurrQues] = useState("Write an Excel formula");
+
+  useEffect(() => {
+    excel ? setCurrQues("Write an Excel formula") : setCurrQues("Write a Python Code") 
+  }, [excel])
 
   const handleQuestionChange = (event) => {
     setCurrQues(event.target.value);
@@ -30,19 +34,20 @@ const DashBorad = () => {
     quesAns !== [] && localStorage.setItem('response', JSON.stringify(quesAns));
   }, [quesAns]);
 
-  const handleClick = async () => {
+  const handleClick = async (Question) => {
     setLoading(true)
-    if (currQues !== '') {
+    console.log(currQues)
+    if (Question !== '') {
       try {
         const response = await openai.createCompletion({
           model: 'text-davinci-003',
-          prompt: currQues,
+          prompt: Question,
           temperature: 0.5,
           max_tokens: 1024,
         });
         setQuesAns((prevVal) => {
           return [...prevVal, {
-            question: currQues
+            question: Question
             , answer: response.data.choices[0].text
           }]
         });
@@ -59,19 +64,31 @@ const DashBorad = () => {
   };
 
   const handleCheckBox = () => {
-    setExcel(!excel) ;
-    setPython(!python) ;
+    setExcel(!excel);
+    setPython(!python);
   }
 
-  const handleCheckBoxClick = () => {
-    excel ? 
-    setCurrQues((prevVal) => {
-      return prevVal + 'in Python'
-    })
-    :
-    setCurrQues((prevVal) => {
-      return prevVal + 'in Excel'
-    })
+  const CheckBoxClick = () => {
+    if(excel){
+      const ques = currQues + ' in Python'
+      setCurrQues(ques) ;
+      handleClick(ques) ;
+    }
+    else{
+      const ques = currQues + ' in Excel'
+      setCurrQues(ques) ;
+      handleClick(ques) ;
+    }
+  }
+
+  const noChats = () => {
+    return (
+      <>
+        <div>
+          No Chats
+        </div>
+      </>
+    )
   }
 
   const handleFunc = (currRes) => {
@@ -79,7 +96,6 @@ const DashBorad = () => {
   }
 
   const messages = (quesAns) => {
-    console.log(quesAns);
     return (
       <>
         <div className="chat-message">
@@ -94,9 +110,9 @@ const DashBorad = () => {
 
         <div className="chat-message">
           <div className="flex items-end">
-              <div className="flex flex-col space-y-2 text-sm max-w-xs mx-2 order-2 items-start">
-                <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">{quesAns.answer !== '' && quesAns.answer}</span></div>
-              </div>
+            <div className="flex flex-col space-y-2 text-sm max-w-xs mx-2 order-2 items-start">
+              <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">{quesAns.answer !== '' && quesAns.answer}</span></div>
+            </div>
             <img src={Bot} className="w-8 h-8 rounded-full order-1" alt="bot" />
           </div>
         </div>
@@ -105,30 +121,25 @@ const DashBorad = () => {
   }
 
   return (
-    <>
+    <>{
       <div className="flex-1 sm:p- justify-between flex flex-col h-screen md:w-4/5 mx-auto">
         <div id="messages" className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch mt-4 md:mt-6 h-3/4">
           <div className="chat-message pt-5">
-          <div className="flex justify-between items-center">
-            <FormGroup className="-space-y-3">
-              <FormControlLabel control={<Checkbox checked={excel}/>} label="Excel" onClick={handleCheckBox}/>
-              <FormControlLabel control={<Checkbox checked={python}/>} label="Python" onClick={handleCheckBox}/>
-            </FormGroup>
-            <Button variant="contained" sx={{height: '40px', marginRight: '5px'}} onClick={handleCheckBoxClick}>{excel ? "Generate Python" : "Generate Excel"}</Button>
-          </div>
-            <div className="flex items-end pt-2">
-              <div className="flex flex-col space-y-2 text-sm max-w-xs mx-2 order-2 items-start">
-                <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">Type Anything..</span></div>
-              </div>
-              <img src={Bot} className="w-8 h-8 rounded-full order-1" alt="bot" />
+            <div className="flex justify-between items-center">
+              <FormGroup className="-space-y-3">
+                <FormControlLabel control={<Checkbox checked={excel} />} label="Excel" onClick={handleCheckBox} />
+                <FormControlLabel control={<Checkbox checked={python} />} label="Python" onClick={handleCheckBox} />
+              </FormGroup>
+              <Button variant="contained" sx={{ height: '40px', marginRight: '5px' }} onClick={CheckBoxClick }>{excel ? "Generate Python" : "Generate Excel"}</Button>
             </div>
           </div>
           {
-            quesAns && (quesAns.slice(-1).answer !== '' && quesAns.slice(-1).answer !== '') &&
+            quesAns && (quesAns.slice(-1).answer !== '' && quesAns.slice(-1).answer !== '') ?
             quesAns.map((response) => {
-              console.log(response);
               return messages(response);
             })
+            :
+            noChats()
           }
         </div>
 
@@ -136,7 +147,7 @@ const DashBorad = () => {
           <div className="flex flex-col md:flex-row space-x-2 mb-4">
             <input type="text" placeholder="Write your message" value={currQues} className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-8 bg-gray-200 rounded-md py-2 border" onChange={handleQuestionChange} />
             <div className="items-center">
-              <button type="button" className="inline-flex items-center justify-center rounded-lg px-4 py-[7px] transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none w-full space-x-1" onClick={handleClick}>
+              <button type="button" className="inline-flex items-center justify-center rounded-lg px-4 py-[7px] transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none w-full space-x-1" onClick={() => {handleClick(currQues)}}>
                 <span className="font-bold md:text-lg">{loading ? "Generating.." : "Generate"}</span>
                 {loading ? null : <RiSendPlane2Fill size='20px' />}
               </button>
@@ -144,7 +155,7 @@ const DashBorad = () => {
           </div>
         </div>
       </div>
-
+      }
       <FixedBottomNavigation chats={quesAns} func={handleFunc} />
     </>
   );
