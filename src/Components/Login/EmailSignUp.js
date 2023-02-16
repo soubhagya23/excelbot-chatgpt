84765b4ab9ab0e8cda74237c898d8c5ed4f11081
soubhagya23 <ranjansoubhagya.master@gmail.com>
2023-02-16 10:25:43 +0530

    Create Account Added

--- src/Components/Login/EmailSignUp.js
+++ src/Components/Login/EmailSignUp.js
@@ -1,106 +1,181 @@
 import React, { useState } from "react";
 import { useAuth } from "../../fireBase/authContext";
 import { useNavigate } from "react-router-dom";
+import EmailSignUp from "./EmailSignUp";
 
-const EmailSignUp = () => {
+const EmailSignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
-  const { signUpWithEmail } = useAuth();
+  const { signInWithEmail } = useAuth();
   const navigate = useNavigate();
+  const [showSignUp, setShowSignUp] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await signUpWithEmail(email, password);
+      await signInWithEmail(email, password);
       navigate("/dashboard");
     } catch (error) {
-      if (error.message === "The password is invalid or the user does not have a password.") {
+      if (
+        error.message ===
+        "The password is invalid or the user does not have a password."
+      ) {
         setError("The password is incorrect.");
       } else {
         setError(error.message);
       }
     }
   };
+
+  const toggleSignUp = () => {
+    setShowSignUp(!showSignUp);
+  };
+
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Your email
-        </label>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          style={{ borderRadius: "13px" }}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-        />
-        <label
-          htmlFor="password"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Your password
-        </label>
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          style={{ borderRadius: "13px" }}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-        />
-        <div className="flex justify-between" style={{ marginTop: 15 }}>
-          <div className="flex items-start">
-            <div className="flex items-center h-5">
-              <input
-                id="remember"
-                type="checkbox"
-                defaultValue
-                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-              />
-            </div>
+      {showSignUp ? (
+        <>
+          <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+            Create a new account
+          </h3>
+          <form onSubmit={handleSubmit}>
             <label
-              htmlFor="remember"
-              className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Remember me
+              Your email
             </label>
-          </div>
-          <a
-            href="#"
-            className="text-sm text-blue-700 hover:underline dark:text-blue-500"
-          >
-            Lost Password?
-          </a>
-        </div>
-        <button
-          style={{ marginTop: 15 }}
-          type="submit"
-          className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Sign In
-        </button>
-        {error && <p>{error}</p>}
-      </form>
-      
-      <div
-        className="text-sm font-medium text-gray-500 dark:text-gray-300"
-        style={{ marginTop: 15 }}
-      >
-        Not registered?{" "}
-        <a
-          href="#"
-          className="text-blue-700 hover:underline dark:text-blue-500"
-        >
-          Create account
-        </a>
-      </div>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              style={{ borderRadius: "13px" }}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            />
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your password
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              style={{ borderRadius: "13px" }}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            />
+            <div
+              className="flex justify-between items-center"
+              style={{ marginTop: 15 }}
+            >
+              <div className="flex items-start">
+                <div className="flex items-center h-5">
+                  <input
+                    id="remember"
+                    type="checkbox"
+                    defaultValue
+                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                  />
+                </div>
+
+                <span className="text-sm text-gray-500 ml-1">Remember me</span>
+              </div>
+              <button
+                className="text-sm font-medium text-blue-600 hover:text-blue-500 dark:hover:text-blue-400"
+                type="button"
+                onClick={toggleSignUp}
+              >
+                Already have an account?
+              </button>
+            </div>
+            <div className="mt-6">
+              <button
+                type="submit"
+                style={{ borderRadius: "13px" }}
+                className="w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:bg-blue-500 dark:focus:ring-offset-gray-800"
+              >
+                Sign Up
+              </button>
+            </div>
+            {error && <p className="mt-4 text-red-500 text-center">{error}</p>}
+          </form>
+        </>
+      ) : (
+        <>
+          <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+            Sign In to our platform
+          </h3>
+          <form onSubmit={handleSubmit}>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              style={{ borderRadius: "13px" }}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            />
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your password
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              style={{ borderRadius: "13px" }}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            />
+            <div
+              className="flex justify-between items-center"
+              style={{ marginTop: 15 }}
+            >
+              <div className="flex items-start">
+                <div className="flex items-center h-5">
+                  <input
+                    id="remember"
+                    type="checkbox"
+                    defaultValue
+                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                  />
+                </div>
+
+                <span className="text-sm text-gray-500 ml-1">Remember me</span>
+              </div>
+              <button
+                className="text-sm font-medium text-blue-600 hover:text-blue-500 dark:hover:text-blue-400"
+                type="button"
+                onClick={toggleSignUp}
+              >
+                Don't have an account?
+              </button>
+            </div>
+            <div className="mt-6">
+              <button
+                type="submit"
+                style={{ borderRadius: "13px" }}
+                className="w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:bg-blue-500 dark:focus:ring-offset-gray-800"
+              >
+                Sign in
+              </button>
+            </div>
+            {error && <p className="mt-4 text-red-500 text-center">{error}</p>}
+          </form>
+        </>
+      )}
     </>
   );
 };
-
-export default EmailSignUp;
+export default EmailSignIn;
